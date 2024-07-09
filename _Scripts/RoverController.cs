5f7810d44f45c380211115126fe9d65be6bb8d8a
ericazhang01 <ericazhang37@gmail.com>
2024-07-09 13:48:06 -0500

    Add files via upload

--- _Scripts/RoverController.cs
+++ _Scripts/RoverController.cs
@@ -1,8 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RoverController : MonoBehaviour
 {
     public float speed = 5f; // Speed of the rover
+    //public Transform spawnArea;
+    public float x;
+    public float y;
+    public float z;
+    void Start() {
+        transform.position = new Vector3(x, y, z);
+    }
 
     void Update()
     {
@@ -11,5 +21,9 @@ public class RoverController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
         transform.position += movement * speed * Time.deltaTime;
+        PlayerPrefs.SetFloat("x", x);
+        PlayerPrefs.SetFloat("y", y);
+        PlayerPrefs.SetFloat("z", z);
     }
+    
 }
