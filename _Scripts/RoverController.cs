d9bed91105719a81ef30b70d174906f302fc1971
ericazhang01 <ericazhang37@gmail.com>
2024-07-15 08:47:58 -0500

    Add files via upload

--- _Scripts/RoverController.cs
+++ _Scripts/RoverController.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 public class RoverController : MonoBehaviour
 {
     public float speed = 5f; // Speed of the rover
-    public Sprite leftFacingSprite;  // Reference to the sprite for the rover facing left
-    public Sprite rightFacingSprite; // Reference to the sprite for the rover facing right
+    private Sprite leftFacingSprite;  // Reference to the sprite for the rover facing left
+    private Sprite rightFacingSprite; // Reference to the sprite for the rover facing right
 
     private SpriteRenderer spriteRenderer;  // Reference to the SpriteRenderer component
+    
 
     void Start()
     {
-        // Get the SpriteRenderer component
         spriteRenderer = GetComponent<SpriteRenderer>();
+        // Get the SpriteRenderer component
+       //spriteRenderer = GetComponent<SpriteRenderer>();
 
         // Set the initial sprite to facing right
-        spriteRenderer.sprite = rightFacingSprite;
+        //spriteRenderer.sprite = rightFacingSprite;
     }
-
+    public void changeLeft( Sprite left ) {
+        leftFacingSprite = left;
+    }
+    public void changeRight( Sprite right) {
+        rightFacingSprite = right;
+    }
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -28,11 +35,16 @@ public class RoverController : MonoBehaviour
         // Check for horizontal input and change the sprite accordingly
         if (moveHorizontal < 0)
         {
-            spriteRenderer.sprite = leftFacingSprite;
+            //transform.eulerAngles = new Vector3(0, 180, 0);
+            //spriteRenderer.sprite = leftFacingSprite;
+            spriteRenderer.flipX = false;
         }
         else if (moveHorizontal > 0)
         {
-            spriteRenderer.sprite = rightFacingSprite;
+            //transform.eulerAngles = new Vector3(0, 0, 0); // Normal
+            //spriteRenderer.sprite = rightFacingSprite;
+            spriteRenderer.flipX = true; 
+
         }
     }
 }
